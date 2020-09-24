17d20b7d94dcc95364ef90a9610b9a3962462d62
Fred-S <fredvantage@gmail.com>
2020-09-24 12:02:55 +0200

    cht css news

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex mb-4" id="news">
     <div class="w-1/2 bg-gray-400 h-12"><h1>Rebuilding the site</h1></div>
-    <div class="w-2/2 bg-gray-500 h-12"><h3>In progress...</h3></div>
+    <div class="w-2/2 bg-gray-500 h-12"><h2>In progress...</h2></div>
   </div>
 </template>
 
@@ -15,7 +15,8 @@ export default {};
   padding: 0 5%;
 }
 
-#news h1 {
+#news h1,
+h2 {
   font-size: 1rem;
 }
 </style>

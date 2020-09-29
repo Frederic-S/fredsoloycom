938c8e06906803fb9023f4cea2dba8aad590b6ed
Fred-S <fredvantage@gmail.com>
2020-09-29 07:46:55 +0200

    added vuegtag

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
+import Vue from "vue";
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from './router'
+import router from "./router";
+import VueGtag from "vue-gtag";
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+  .use(router)
+  .mount("#app");
+
+Vue.use(VueGtag, {
+  config: { id: "UA-91636249-3" },
+});

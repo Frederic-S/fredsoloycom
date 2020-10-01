d54f337f84820b6af35b4895065452dd239a116e
Fred-S <fredvantage@gmail.com>
2020-10-01 16:22:35 +0200

    cht gtag version for cli3

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
-import Vue from 'vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueGtag from 'vue-gtag'
+import gtag from 'vue-gtag'
 
 createApp(App)
   .use(router)
   .mount('#app')
 
-Vue.use(
-  VueGtag,
-  {
-    config: { id: 'UA-91636249-3' },
+App.use(gtag, {
+  property: {
+    id: 'UA-91636249-3',
   },
-  router
-)
+})
